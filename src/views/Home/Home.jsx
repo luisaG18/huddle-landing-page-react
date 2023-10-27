6377c5091042b1924cee467b1b22afcbc1d62087
Luisa Giraldo <luiisagiraldori@gmail.com>
2023-10-26 22:39:34 -0500

    Add images curve

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,6 +5,14 @@ import ImgFlowingConversation from "../../assets/images/illustration-flowing-con
 import ImgYourUsers from "../../assets/images/illustration-your-users.svg";
 import IconCommunities from "../../assets/icons/icon-communities.svg";
 import IconMessages from "../../assets/icons/icon-messages.svg";
+import BgSectionTop1Mobile from "../../assets/images/backgrounds/bg-section-top-mobile-1.svg";
+import BgSectionBottom1Mobile from "../../assets/images/backgrounds/bg-section-bottom-mobile-1.svg";
+import BgSectionTop2Mobile from "../../assets/images/backgrounds/bg-section-top-mobile-2.svg";
+import BgSectionBottom2Mobile from "../../assets/images/backgrounds/bg-section-bottom-mobile-2.svg";
+import BgSectionTop1Desktop from "../../assets/images/backgrounds/bg-section-top-desktop-1.svg";
+import BgSectionBottom1Desktop from "../../assets/images/backgrounds/bg-section-bottom-desktop-1.svg";
+import BgSectionTop2Desktop from "../../assets/images/backgrounds/bg-section-top-desktop-2.svg";
+import BgSectionBottom2Desktop from "../../assets/images/backgrounds/bg-section-bottom-desktop-2.svg";
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
@@ -47,17 +55,23 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className="container-section container-section--diferent container-section--diferent--reverse">
-        <img src={ImgGrowTogether} alt="Image Screen Mockups" />
-        <div className="container-section--text">
-          <h1>Grow Together</h1>
-          <span>
-            Generate meaningful discussions with your audience and build a
-            strong, loyal community. Think of the insightful conversations you
-            miss out on with a feedback form.
-          </span>
-        </div>
-      </section>
+      <div className="img-curve">
+        <img className="img-top-m" src={BgSectionTop1Mobile} />
+        <img className="img-top-d" src={BgSectionTop1Desktop} />
+        <section className="container-section container-section--diferent container-section--diferent--reverse">
+          <img src={ImgGrowTogether} alt="Image Screen Mockups" />
+          <div className="container-section--text">
+            <h1>Grow Together</h1>
+            <span>
+              Generate meaningful discussions with your audience and build a
+              strong, loyal community. Think of the insightful conversations you
+              miss out on with a feedback form.
+            </span>
+          </div>
+        </section>
+        <img className="img-bottom-m" src={BgSectionBottom1Mobile} />
+        <img className="img-bottom-d" src={BgSectionBottom1Desktop} />
+      </div>
       <section className="container-section container-section--diferent">
         <img src={ImgFlowingConversation} alt="Image Screen Mockups" />
         <div className="container-section--text">
@@ -69,17 +83,23 @@ function Home() {
           </span>
         </div>
       </section>
-      <section className="container-section container-section--diferent container-section--diferent--reverse">
-        <img src={ImgYourUsers} alt="Image Screen Mockups" />
-        <div className="container-section--text">
-          <h1>Your Users</h1>
-          <span>
-            It takes no time at all to integrate Huddle with your app`s
-            authentication solution. This means, once signed in to your app,
-            your users can start chatting immediately.
-          </span>
-        </div>
-      </section>
+      <div className="img-curve">
+        <img className="img-top-m" src={BgSectionTop2Mobile} />
+        <img className="img-top-d" src={BgSectionTop2Desktop} />
+        <section className="container-section container-section--diferent container-section--diferent--reverse img-curve1">
+          <img src={ImgYourUsers} alt="Image Screen Mockups" />
+          <div className="container-section--text">
+            <h1>Your Users</h1>
+            <span>
+              It takes no time at all to integrate Huddle with your app`s
+              authentication solution. This means, once signed in to your app,
+              your users can start chatting immediately.
+            </span>
+          </div>
+        </section>
+        <img className="img-bottom-m" src={BgSectionBottom2Mobile} />
+        <img className="img-bottom-d" src={BgSectionBottom2Desktop} />
+      </div>
       <section className="container-section container-section--final container-section--diferent">
         <h1>Ready To Build Your Community?</h1>
         <Button
